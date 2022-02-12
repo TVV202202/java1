579b7b21b871616295e8a9fd32a9b42a18e639e1
VT <victor.v.tarasov@yandex.ru>
2022-02-13 00:42:02 +0300

    HW-15

--- src/ru/progwards/java1/lessons/classes1/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes1/ComplexNum.java
@@ -10,29 +10,41 @@ public class ComplexNum {
     }
 
     public String toString() {
-        return a + "+" + b + "i";
+        if (b > 0)
+            return a + "+" + b + "i";
+        else if (b < 0)
+            return a + "" + b + "i";
+        else
+            return Integer.toString(a);
     }
-    public ComplexNum add(ComplexNum num){
-        return new ComplexNum(this.a+num.a,this.b+num.b);
+
+    public ComplexNum add(ComplexNum num) {
+        return new ComplexNum(this.a + num.a, this.b + num.b);
     }
-    public ComplexNum sub(ComplexNum num){
-        return new ComplexNum(this.a-num.a,this.b-num.b);
+
+    public ComplexNum sub(ComplexNum num) {
+        return new ComplexNum(this.a - num.a, this.b - num.b);
     }
-    public ComplexNum mul(ComplexNum num){
+
+    public ComplexNum mul(ComplexNum num) {
         // (a + bi) * (c + di) = (a*c - b*d) + (b*c + a*d)i
-        return new ComplexNum(this.a*num.a - this.b*num.b,this.b*num.a+this.a*num.b);
+        return new ComplexNum(this.a * num.a - this.b * num.b, this.b * num.a + this.a * num.b);
     }
-    public ComplexNum div(ComplexNum num){
+
+    public ComplexNum div(ComplexNum num) {
         // (a + bi) / (c + di) = (a*c + b*d)/(c*c+d*d) + ((b*c - a*d)/(c*c+d*d))i
-        int a = (int)((double)(this.a*num.a + this.b*num.b) / (num.a*num.a + num.b*num.b));
-        int b = (int)((double)(this.b*num.a - this.a*num.b) / (num.a*num.a + num.b*num.b));
+        // не понимаю, если условие, что a и b должны быть целые, то получается целочисленное деление комплексных чисел?
+        int a = (this.a * num.a + this.b * num.b) / (num.a * num.a + num.b * num.b);
+        int b = (this.b * num.a - this.a * num.b) / (num.a * num.a + num.b * num.b);
         return new ComplexNum(a, b);
     }
+
     public static void main(String[] args) {
-        ComplexNum x=new ComplexNum(7,4);
-        ComplexNum y=new ComplexNum(4, 6);
+        ComplexNum x = new ComplexNum(7, 4);
+        ComplexNum y = new ComplexNum(4, 6);
         System.out.println(x);
         System.out.println(x.add(y));
+        System.out.println(x.sub(y));
         System.out.println(x.mul(y));
         System.out.println(x.div(y));
 
