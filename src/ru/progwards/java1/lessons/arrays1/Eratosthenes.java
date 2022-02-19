cae4544d7a1774358601be81721ac4d75fc157ea
VT <victor.v.tarasov@yandex.ru>
2022-02-19 22:34:21 +0300

    tt

--- src/ru/progwards/java1/lessons/arrays1/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays1/Eratosthenes.java
@@ -1,13 +1,13 @@
 package ru.progwards.java1.lessons.arrays1;
 
+import java.util.Arrays;
+
 public class Eratosthenes {
     private boolean[] sieve;
 
     public Eratosthenes(int N) {
         sieve = new boolean[N + 1];
-        for (int i = 0; i <= N; i++) {
-            sieve[i] = true;
-        }
+        Arrays.fill(sieve, true);
         sift();
     }
 
@@ -28,9 +28,9 @@ public class Eratosthenes {
 
     public static void main(String[] args) {
         Eratosthenes erat = new Eratosthenes(100);
-        int n = 1;
+        int n = 7;
         if (n>0 && n<=100)
-            System.out.println(erat.isSimple(1));
+            System.out.println(erat.isSimple(n));
         else
             System.out.println("n вне диапазона");
     }
