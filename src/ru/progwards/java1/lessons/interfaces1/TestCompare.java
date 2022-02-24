6b0f68025c2e6b4b8689ea77745e5d679459abd1
VT <victor.v.tarasov@yandex.ru>
2022-02-24 13:36:01 +0300

    HW 21

--- src/ru/progwards/java1/lessons/interfaces1/TestCompare.java
+++ src/ru/progwards/java1/lessons/interfaces1/TestCompare.java
@@ -6,7 +6,7 @@ public class TestCompare {
     public static void main(String[] args) {
         Animal[] animals = new Animal[6];
         animals[0] = new Cow("Пеструшка", 350);
-        animals[1] = new Cow("Рыжая", 520);
+        animals[1] = new Cow("Рыжая", 250);
         animals[2] = new Duck("Даша", 3);
         animals[3] = new Duck("Маша", 3.5);
         animals[4] = new Hamster("Акакий", 0.25);
