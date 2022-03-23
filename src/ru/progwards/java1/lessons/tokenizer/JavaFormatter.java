e5e9fc51dd4a3ddf5edc0846612c3c94e1f125bf
VT <victor.v.tarasov@yandex.ru>
2022-03-23 03:07:18 +0300

    HW 25

--- src/ru/progwards/java1/lessons/tokenizer/JavaFormatter.java
+++ src/ru/progwards/java1/lessons/tokenizer/JavaFormatter.java
@@ -29,14 +29,10 @@ public class JavaFormatter {
 
             // если открывающая или закрывающая квадратная скобка  -> склеиваем с предыдущим
 
-            // для прототы понимания и отладки разнес круглые, квадратные и знаки по разным методам,
-            // можно слить в один (циклов будет в 3 раза меньше), но надо ли...
-            str = handlingBracket(str, operator);
-            str = handlingSquareBracket(str);
-            str = handlingBrace(str);
-            str = handlingSign(str);
+            // для прототы понимания и отладки разнес круглые, квадратные и знаки по разным методам
 
-            str = str.replace(" ;", ";");
+            str = handlingSign(str, operator);
+
             str = str.replace(" .", ".");
             str = str.replace(". ", ".");
 
@@ -83,128 +79,122 @@ public class JavaFormatter {
         return "    ".repeat(Math.max(0, count));
     }
 
-    private static String handlingBracket(String str, boolean operator) { // если метод, то true, если оператор, то false
-        char[] chArr = str.toCharArray(); // разбиение строки на символы
-        for (int j = 0; j < chArr.length; j++) {
-            switch (chArr[j]) {
-                case '(' -> {
-                    str = str.replace(chArr[j] + " ", String.valueOf(chArr[j])); // всегда убираем пробел после (
-                    //str = str.replace(String.valueOf(chArr[j]), " " + chArr[j]); // добавляем пробел спереди для операторов
-                    //str = str.replace("  ", " "); // убираем двойные пробелы
-                    if (!operator) { // если метод, то убираем пробелы спереди
-                        str = str.replace(" " + chArr[j], String.valueOf(chArr[j]));
-                    }
+    private static String handlingBracket(String str, char ch, boolean operator) { // если метод, то true, если оператор, то false
+        switch (ch) {
+            case '(' -> {
+                str = str.replace(ch + " ", String.valueOf(ch)); // всегда убираем пробел после (
+                //str = str.replace(String.valueOf(chArr[j]), " " + chArr[j]); // добавляем пробел спереди для операторов
+                //str = str.replace("  ", " "); // убираем двойные пробелы
+                if (!operator) { // если метод, то убираем пробелы спереди
+                    str = str.replace(" " + ch, String.valueOf(ch));
                 }
-                case ')' -> str = str.replace(" " + chArr[j], String.valueOf(chArr[j])); // всегда убираем пробел перед )
-                default -> {
-                }
+            }
+            case ')' -> str = str.replace(" " + ch, String.valueOf(ch)); // всегда убираем пробел перед )
+            default -> {
             }
         }
         return str;
     }
 
-    private static String handlingSquareBracket(String str) {
-        char[] chArr = str.toCharArray(); // разбиение строки на символы
-        for (int j = 0; j < chArr.length; j++) {
-            switch (chArr[j]) {
-                case '[' -> {
-                    str = str.replace(chArr[j] + " ", String.valueOf(chArr[j])); // всегда убираем пробел после [
-                    str = str.replace(" " + chArr[j], String.valueOf(chArr[j])); // всегда убираем пробел перед [
-                }
-                case ']' -> str = str.replace(" " + chArr[j], String.valueOf(chArr[j])); // всегда убираем пробел перед ]
-                default -> {
-                }
+    private static String handlingSquareBracket(String str, char ch) {
+        switch (ch) {
+            case '[' -> {
+                str = str.replace(ch + " ", String.valueOf(ch)); // всегда убираем пробел после [
+                str = str.replace(" " + ch, String.valueOf(ch)); // всегда убираем пробел перед [
+            }
+            case ']' -> str = str.replace(" " + ch, String.valueOf(ch)); // всегда убираем пробел перед ]
+            default -> {
             }
         }
         return str;
     }
 
-    private static String handlingBrace(String str) {
-        char[] chArr = str.toCharArray(); // разбиение строки на символы
-        for (int j = 0; j < chArr.length; j++) {
-            switch (chArr[j]) {
-                case '{', '}' -> {
-                    if (j != chArr.length - 1) {
-                        str = str.replace(chArr[j] + " ", String.valueOf(chArr[j])); //  убираем пробел после {
-                        str = str.replace(" " + chArr[j], String.valueOf(chArr[j])); //  убираем пробел перед {
-                    }
-
+    private static String handlingBrace(String str, char ch) {
+        switch (ch) {
+            case '{', '}' -> {
+                if (ch != str.charAt(str.length() - 1)) {
+                    str = str.replace(ch + " ", String.valueOf(ch)); //  убираем пробел после {
+                    str = str.replace(" " + ch, String.valueOf(ch)); //  убираем пробел перед {
                 }
-                default -> {
-                }
+            }
+            default -> {
             }
         }
         return str;
     }
 
-    private static String handlingSign(String str) {
-        char[] chArr = str.toCharArray(); // разбиение строки на символы
-        for (int j = 0; j < chArr.length; j++) {
-            switch (chArr[j]) {
-                case '+', '-', '/', '*', '=', '<', '>', '!', '%' -> {
-                    if (j + 1 < chArr.length) {
-                        switch (chArr[j + 1]) {
-                            case '+', '-' -> {
-                                String tmp = chArr[j] + String.valueOf(chArr[j + 1]);
-                                str = str.replace(" " + tmp, tmp);
-                                str = str.replace(tmp + " ", tmp);
-                                j++;
-                            }
-                            case '=' -> {
-                                String tmp = chArr[j] + String.valueOf(chArr[j + 1]);
-                                str = str.replace(" " + tmp + " ", tmp); // убираем лишние пробелы
-                                str = str.replace(" " + tmp, tmp);
-                                str = str.replace(tmp + " ", tmp);
-                                str = str.replace(tmp, " " + tmp + " "); // ставим нужные пробелы
-                                j++;
-                            }
-                            default -> {
-                                str = str.replace(" " + chArr[j] + " ", String.valueOf(chArr[j]));
-                                str = str.replace(" " + chArr[j], String.valueOf(chArr[j]));
-                                str = str.replace(chArr[j] + " ", String.valueOf(chArr[j]));
-                                str = str.replace(String.valueOf(chArr[j]), " " + chArr[j] + " ");
+
+    private static String handlingSign(String str, boolean operator) {
+        String[] tmpStr = str.split("\""); // разбиваем строку на подстроки с "", чтобы не обрабатывать "строковые части"
+        for (int i = 0; i < tmpStr.length; i += 2) { // пробегаем через один, чтобы не обрабатывать "строковые части"
+            char[] chArr = tmpStr[i].toCharArray(); // разбиение строки на символы
+            for (int j = 0; j < chArr.length; j++) {
+                tmpStr[i] = handlingBracket(tmpStr[i], chArr[j], operator); // круглые скобки
+                tmpStr[i] = handlingSquareBracket(tmpStr[i], chArr[j]); // квадратные скобки
+                tmpStr[i] = handlingBrace(tmpStr[i], chArr[j]); // фигурные скобки
+                switch (chArr[j]) {
+                    case '+', '-', '/', '*', '=', '<', '>', '!', '%' -> {
+                        if (j + 1 < chArr.length) {
+                            switch (chArr[j + 1]) {
+                                case '+', '-' -> {
+                                    String tmp = chArr[j] + String.valueOf(chArr[j + 1]);
+                                    if (j - 1 > 0 && chArr[j - 2] != ';') {
+                                        tmpStr[i] = tmpStr[i].replace(" " + tmp, tmp);
+                                    }
+                                    tmpStr[i] = tmpStr[i].replace(tmp + " ", tmp);
+                                    j++;
+                                }
+                                case '=' -> {
+                                    String tmp = chArr[j] + String.valueOf(chArr[j + 1]);
+                                    tmpStr[i] = tmpStr[i].replace(tmp, " " + tmp + " "); // ставим нужные пробелы
+                                    j++;
+                                }
+                                default -> {
+                                    tmpStr[i] = tmpStr[i].replace(String.valueOf(chArr[j]), " " + chArr[j] + " ");
+                                }
                             }
                         }
-                        //str = str.replace("  ", " ");
+                    }
+                    case ',' -> {
+                        tmpStr[i] = tmpStr[i].replace(" , ", ",");
+                        tmpStr[i] = tmpStr[i].replace(" ,", ",");
+                        tmpStr[i] = tmpStr[i].replace(", ", ",");
+                        tmpStr[i] = tmpStr[i].replace(",", ", ");
+                    }
+                    case ';' -> {
+                        tmpStr[i] = tmpStr[i].replace(" ;", ";");
+                        if (j + 1 < chArr.length && !Character.isWhitespace(chArr[j + 1])) {
+                            tmpStr[i] = tmpStr[i].replace(";", "; ");
+                        }
+                    }
+                    default -> {
                     }
                 }
-                case ',' -> {
-                    str = str.replace(" , ", ",");
-                    str = str.replace(" ,", ",");
-                    str = str.replace(", ", ",");
-                    str = str.replace(",", ", ");
-                }
-                default -> {
-                }
+                tmpStr[i] = delDoubleSpace(tmpStr[i]);
             }
+        }
+        return String.join("\"", tmpStr);
+    }
+
+    private static String delDoubleSpace(String str) {
+        while (str.contains("  ")) {
+            str = str.replace("  ", " ");
         }
         return str;
     }
 
     public static void main(String[] args) {
-        String code = "public static void main(String  []  args) {\n" +
-                "    int row=2 , column=   3;\n" +
-                "    int [ ] [ ] matrix = { { 2,3,4 } , { 5,6,4 } };\n" +
-                "\n" +
-                "    display ( matrix );\n" +
-                "\n" +
-                "    int [ ] [ ] transpose = new int [ column ] [ row ] ;\n" +
-                "        for(int i=0; i < row; i++) {\n" +
-                "            for ( int j = 0; j<column; j ++) {\n" +
-                "                transpose [ j ] [ i ] = matrix [ i ] [ j ] ;}\n" +
-                "     }\n" +
-                "\n" +
-                "  display(transpose);\n" +
-                "}\n" +
-                "\n" +
-                "public static void display(int[][] matrix) {\n" +
-                "    System.out.println( \"The matrix is: \" ) ;\n" +
-                "  for(int [ ] row : matrix)\n" +
-                "  {\n" +
-                "     for (int column : row) {\n" +
-                "            System.out.print( column + \"    \") ;\n" +
-                "        }\n" +
-                "     System.out.println ();    }}";
+        String code = "public static void main(String    []    args   )\n" +
+                "   {\n" +
+                "     int num = 1234 , reversed = 0;\n" +
+                "  System.out.println(\"Original Number: \" + num   );\n" +
+                "    while(num != 0)\n" +
+                " {\n" +
+                "        int digit = num%10;\n" +
+                "        reversed=reversed*10+    digit ;\n" +
+                "        num /= 10;\n" +
+                "    }\n" +
+                "    System.out.println(\"Reversed Number: \" + reversed);}";
         String code1 = format(code);
         System.out.println(code1);
 
